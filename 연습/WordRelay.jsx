d89cae2c96ab1dccbb8ab97d8355cf3a1017a494
Taewoong1378 <ktw2378@korea.ac.kr>
2021-07-14 11:25:09 +0900

    .

--- 연습/WordRelay.jsx
+++ 연습/WordRelay.jsx
@@ -1,19 +1,18 @@
 const React = require('react');
 const { useState, useRef } = React;
 
-
+const e = React.createElement;
 const WordRelay = () => {
-
     const [word, setWord] = useState('제로초');
-    const [value, setValue] = useState('');
     const [result, setResult] = useState('');
+    const [value, setValue] = useState('');
     const inputRef = useRef();
 
-    const onSubmitForm = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if(word.substr(-1) === value.substr(0,1)){
-            setWord(value);
+        if(word.substr(-1)===value.substr(0,1)){
             setResult('딩동댕');
+            setWord(value);
             setValue('');
             inputRef.current.focus();
         } else {
@@ -23,21 +22,20 @@ const WordRelay = () => {
         }
     }
 
-    const onChangeInput = (e) => {
+    const onChange = (e) => {
         setValue(e.target.value);
     }
-
     return (
         <>
-        <div>{word}</div>
-        <form onSubmit={onSubmitForm}>
-            <label htmlFor="taewoong">글자를 입력하세요 : </label>
-            <input ref={inputRef} type="text" onChange={onChangeInput} value={value} />
-            <button>입력!</button>
-        </form>
-        <div>{result}</div>
+            <div>{word}</div>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="taewoong"></label> 글자를 입력하세요 : 
+                <input ref={inputRef} value={value} onChange={onChange} type="text" id="taewoong" />
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
         </>
-    )
+    );
 }
 
 module.exports = WordRelay;
